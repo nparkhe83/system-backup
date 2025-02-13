976674728304fdf50a7d39344247897d9e0ea1fe
Nilesh Parkhe <parkhenilesh@outlook.com>
2025-02-13 17:18:13 +0530

    Refactor Brew config
    - Brew not found error message not showing up anymore.

--- install_Mac.sh
+++ install_Mac.sh
@@ -1,17 +1,20 @@
 # Check if Homebrew is installed
 if ! command -v brew &>/dev/null; then
-  echo "Homebrew is not installed. Installing Homebrew..."
+  echo "\n\nHomebrew is not installed. Installing Homebrew..."
 
   # Install Homebrew
   # Run the following command as current user so that the Homebrew installation script can be run without any user confirmation prompt
   sudo -u $(logname) /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  brew update
 
   echo 'export PATH=$PATH:/opt/homebrew/bin' >>~/.zshrc
   source ~/.zshrc
+
+  echo "\n\nUpdating Homebrew..."
+  brew update
 fi
 
 # Install Ansible
+echo "\n\nInstalling Ansible..."
 brew install ansible
 echo "\n\n"
 
